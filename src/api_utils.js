db21bad6daa6bfe09575c44eafdee8ff1d3d96ae
Mohtasheem <mohtasheemejaz@gmail.com>
2022-07-06 20:12:23 +0530

    Home_API

--- src/api_utils.js
+++ src/api_utils.js
@@ -1,6 +1,4 @@
 export function getHomeData() {
     // return fetch('https://qoolqatar.com/qool_qatar/api/homedata')
     return fetch('https://qoolqatar.com/qool_qatar/api/homedata').then((response) => response.json())
-    // return fetch('https://newsapi.org/v2/everything?q=bitcoin&apiKey=f688b40cbae84bbb9364589714dc2da6').then((response) => response.json())
-    // return fetch('https://newsapi.org/v2/everything?q=bitcoin&apiKey=f688b40cbae84bbb9364589714dc2da6')
 }
